5fae3ac362a42b8071d4c351816a0577c74bc31e
yashvi-param <yashvirwn123@gmail.com>
2025-04-10 13:57:39 +0530

    Add files via upload
    day-3 complete

---
+++ day-3/index.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //arithmatic operators
+    // int a;
+    // int b; 
+    // cin >> a;
+    // cin >> b;
+
+    // cout << "addition of a & b =  " << a+b << endl;
+    // cout << "substraction of a & b = " << a-b << endl;
+    // cout << "devidation of a & b = " << a/b << endl;
+    // cout << "multiplication of a & b = " << a*b << endl;
+    // cout << "moduls of a & b ="<< a%b << endl;
+
+//relational operators 
+
+    // int x=10;
+    // int y=20;
+    // int z=5;
+
+  // x < y  true
+ // x > y  false
+ // x == y  false
+ // x <= y  true
+ // x >= y false
+ // x != y true
+
+ //logical operators
+
+ //  10 == 20 && 20 == 20; false
+//  10 == 20 || 20 == 20; true
+//  10 == 20 || 20 == 30; true
+
+//  x < y && y < z
+//  x < y && y < z
+
+//Assignment operators 
+ 
+// int x=10;
+//      int y=20;
+//    int z=5;
+
+
+//     z=x+y;
+
+//     int a = 100;
+//     int b = 20;
+
+//     b=a+b;
+//     b+=a;
+
+//int num=73;
+
+
+//cout << sizeof (b) << endl;
+//cout << sizeof (num) << endl;
+
+
+//int n=20;
+//int n2=20;
+//n++; //21
+//n++; //22
+//n++; //23
+
+// --n2; //19
+// --n2;//18
+// n2--;//17
+// 
+// int num3 = 5;
+// int num4 = 5;
+// 
+// cout << --num3 << endl;
+// cout << num4-- << endl;
+// cout << --num4 << endl;
+// 
+// cout << n << " " << n2 << endl;
+
+cout <<  1 + 4 * 2 - 3 / 3 + 2 * 2 / 4 << endl;
+
+
+return 0;
+}
+ 
+
+
+
