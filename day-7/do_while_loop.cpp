c2f14c5ead32ddb1eecf487d318deaf2dabf7c94
yashvi-param <yashvirwn123@gmail.com>
2025-05-08 10:50:16 +0530

    Add files via upload
    complete

---
+++ day-7/do_while_loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i = 1;
+
+
+    do
+    {
+        
+        i++;
+        cout << i << endl;
+
+    }
+    while(i < 5);
+
+
+
+
+
+
+    return 0;
+}
+
