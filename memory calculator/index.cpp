9b98b699e1ddd0ad88e4d1598fa09b30eb0af4e2
yashvi-param <yashvirwn123@gmail.com>
2025-05-14 17:52:52 +0530

    Add files via upload
    complete memory calculate

---
+++ memory calculator/index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class student{
+   
+    public: 
+    int id;
+    string name;
+};
+
+int main(){
+    student std1, std2;
+
+    vector<student<int>list>
+    list.push_back(1, "deepali");
+    list.push_back(2, "nayana");
+
+
+
+    for (int i=0; i<5; i++)
+    {
+        cout << "plese enter details such as name, ID...!" << endl;
+        cout << "student ID : " << endl;
+        cin>> ptr->stdid;
+        cout << "name : " << endl;
+        cin >> ptr->name;
+    }
+
+    return 0;
+}
